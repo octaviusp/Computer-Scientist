9fcfc092dded483e48dcf35be9c23e92858710d1
Octavio Pavon <octaviopavon7@gmail.com>
2022-06-21 22:58:36 -0300

    Update ALL_SORTING_ALGORITHMS.py

--- Python/ALL_SORTING_ALGORITHMS.py
+++ Python/ALL_SORTING_ALGORITHMS.py
@@ -10,11 +10,11 @@ print("- INITIAL VECTOR WITHOUT ORDER ", get_arr(), " - ")
 
 #  BUBBLE SORT
 def bubble_sort(array):
-    
+    # RECORREMOS POR CADA ELEMENTO DEL ARRAY 
     for x in range(0,len(array)):
-        
+        # RECORREMOS POR CADA ELEMENTO CADA ELEMENTO, POR ESO ES N^2
         for y in range(0,len(array)-1):
-            
+            # si encontramos diferencia, swapeamos valores
             if array[x]<array[y]:
                 aux = array[x]
                 array[x] = array[y]
@@ -28,16 +28,21 @@ print("\n - BUBBLE SORT \n SPECIFICATIONS: \n Average runtime: O(n^2) - \n ",bub
 
 # INSERTION SORT
 def insertion_sort(array):
-    
+    # recorremos para cada elemtno del array
     for x in range(0, len(array)):
-        
+        # creamos un puntero donde es menor a la posicion actual
         j = x-1
+        # creamos una KEY que es el valor de la posicion actual, para mantenerlo CONSTANTE
         KEY = array[x]
-        
+        # mientras que la KEY sea menor al valor anterior osea j donde j = x-1, y j sea mayor a 0 osea haya elementos a la izquierda de X para comparar
         while KEY<array[j] and j >= 0:
+            # swapeamos pero sin cambiar el valor de KEY que es el constante, a key lo cambiaremos como array[j+1]
+            # ya que si j = x-1 por lo tanto j+1 = x
             aux = KEY
             array[j+1] = array[j]
             array[j] = aux
+            #reducimos j en 1 IMPORTANTISIMO, ya que esto lograra que el numero se compare con TODOS los de su izquierda.
+            # luego asi, haremos insertion sort
             j -= 1
             
     print(f"\n | Ordered: {is_ordered(array)}|")
@@ -47,16 +52,21 @@ print("\n - INSERTION SORT \n SPECIFICATIONS: \n Average runtime: O(n^2) - \n ",
 
 # SELECTION SORT
 def selection_sort(array):
-    
+    # recorremos cada elemtno del array
     for x in range(0, len(array)):
-        
+        # seteamos un valor MINIMO, obvio es hardcodeado, podria ser mas alto!
         min_value = 999
+        # iniciamos posicion en 0
         pos = 0
+        # recorremos por cada elemento X, pero iniciamos en la posicion del elemento X, cosa que creemos una barrera toda para adelante y reduzca 
+        # el tiempo de ejecucion
         for y in range(x, len(array)):
+            # si el elemento de adelante es menor a min value, seteamos min value a este y guardamos la posicion donde fue el min value
+            # esto es para comparar un unmero constante contra todos los de su derecha y buscar el MINIMO de todos estos
             if array[y] < min_value:
                 min_value = array[y]
                 pos = y
-
+        # una vez terminada la busqueda del minimo, comparamos que realmente sea menor al actual y si es asi, SWAPEAMOS!
         if array[x] > min_value:
             aux = array[x]
             array[x] = array[pos]
@@ -69,12 +79,13 @@ print("\n - SELECTION SORT \n SPECIFICATIONS: \n Average runtime: O(n^2) - \n ",
 #####################################################
 #               DIVIDE AND CONQUEROR                #
 #####################################################
-
+# Divide y conquistarás, la metodología para realizar operaciones en Log (n) y obtener un excelente perfomance y algoritmos escalables.
 # QUICK SORT
 
 def quick_sort(array, low, high):
+    # comprobamos que low < high, caso base, si se cumple
     if low < high:
-        
+        # creamos una particion
         PartitionIndex = partition(array, low, high)
         
         quick_sort(array, low, PartitionIndex-1)
